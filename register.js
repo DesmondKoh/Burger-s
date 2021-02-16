ddf31dc6b3baf83080289272ead9d45af7cfe445
herman088 <hermancho08@gmail.com>
2021-02-16 18:25:06 +0800

    Add Register Script

---
+++ register.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function () {
+ const APIKEY = "601acf656adfba69db8b6d25";
+
+ let username = $("#username").val();
+ let password = $("#password").val();
+ let point = 0;
+
+ $("#register").on("click", function () {
+
+ 
+ let jsondata = {
+    "username":username,
+    "password": password,
+    "point":point
+
+     
+  };
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://burgers-e911.restdb.io/rest/account",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(jsondata)
+
+  }
+
+ $.ajax(settings).done(function (response) {
+    console.log(response);
+})
+
+})
+
+})
