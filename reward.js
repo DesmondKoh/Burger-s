1b19e9ef6761b57c8e7220d6ffb4192fbb076356
herman088 <hermancho08@gmail.com>
2021-02-17 22:34:27 +0800

    Delete Contact and Edit Reward Script

--- reward.js
+++ reward.js
@@ -1,13 +1,98 @@
 
-function rotateFunction(){
-  var min = 1080;
-  var max = 5400;
-  var deg = Math.floor(Math.random() * (max - min)) + min;
-  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
 
-    
+
+
+
+
+ 
+function rotateFunction(){
+  var spinsLeft = 1 ;
+  const APIKEY = "601acf656adfba69db8b6d25";
+  var couponId;
+  var couponName;  
+  var username = 'herman01';
+  
+  
+  var possibleNumbersArray = [1800,1900,2050,2475];
+  randomNum = Math.round(Math.random() * 4);
+  
+  var deg = possibleNumbersArray[randomNum];
+  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+ 
+
+  if  (deg = 1800)
+      {
+        couponId = 2;
+        couponName = '$4Coupon';
+      }
+
+  
+  else if (deg == 1900)
+      {
+        couponId = 4;
+        couponName = '20%Off';
+
+
+
+      }
+      
+
+
+  else if (deg == 2050)
+  {
+    couponId = 3;
+    couponName = '10%Off';
+
+
+
+  }
+
+
+  else if (deg == 2475)
+  {
+    couponId = 1;
+    couponName = '$2Coupon';
+
+
   }
   
+  
+
+
+
+  
+  
+  let jsondata = {
+    "couponId":couponId,
+    "username":username
+    
+
+  };
+
+  let settings = {
+    "async": true,
+    "crossDomain": true, 
+    "url": "https://burgers-e911.restdb.io/rest/reward",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache"
+    },
+    "processData": false,
+    "data": JSON.stringify(jsondata)
+
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+})
+
+
+  spinsLeft -= 1;
+
+
+}
 
  
 
