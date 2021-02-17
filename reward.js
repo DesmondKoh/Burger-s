0a97f78f726b061e7dc6d378af08356dee38dd45
DesmondKoh <74166108+DesmondKoh@users.noreply.github.com>
2021-02-17 23:15:36 +0800

    Fix bug

--- reward.js
+++ reward.js
@@ -1,17 +1,34 @@
-
-
-
-
-
-
+$(document).ready(function () {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://burgers-e911.restdb.io/rest/account",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    for(let i = 0; i < response.length; i++){
+      let account = $("#navbarDropdownMenuLink").text()
+      if(account == "Account"){
+        $(".current-point").html("<p>Please login to view points,</p>")   
+      }
+      else if(account == response[i].username){   
+        $(".current-point").html("<p>You have " + response[i].point + " points</p>")  
+      }
+    }
+  });
+})
  
 function rotateFunction(){
   var spinsLeft = 1 ;
-  const APIKEY = "601acf656adfba69db8b6d25";
   var couponId;
   var couponName;  
-  var username = 'herman01';
-  
+  let account = $("#navbarDropdownMenuLink").text()
   
   var possibleNumbersArray = [1800,1900,2050,2475];
   randomNum = Math.round(Math.random() * 4);
@@ -19,54 +36,31 @@ function rotateFunction(){
   var deg = possibleNumbersArray[randomNum];
   document.getElementById('box').style.transform = "rotate("+deg+"deg)";
  
-
-  if  (deg = 1800)
-      {
-        couponId = 2;
-        couponName = '$4Coupon';
-      }
-
-  
-  else if (deg == 1900)
-      {
-        couponId = 4;
-        couponName = '20%Off';
-
-
-
-      }
-      
-
-
-  else if (deg == 2050)
+  if (deg <= 1800)
+  {
+    couponId = 2;
+    couponName = '$4 OFF';
+  } 
+  else if (deg <= 1900)
+  {
+    couponId = 4;
+    couponName = '20% OFF';
+  }
+  else if (deg <= 2050)
   {
     couponId = 3;
-    couponName = '10%Off';
-
-
-
+    couponName = '10% OFF';
   }
-
-
-  else if (deg == 2475)
+  else if (deg <= 2475)
   {
     couponId = 1;
-    couponName = '$2Coupon';
-
-
+    couponName = '$2 OFF';
   }
-  
-  
-
-
-
   
   
   let jsondata = {
     "couponId":couponId,
-    "username":username
-    
-
+    "username": account
   };
 
   let settings = {
@@ -85,13 +79,8 @@ function rotateFunction(){
   }
 
   $.ajax(settings).done(function (response) {
-    console.log(response);
-})
-
-
+  })
   spinsLeft -= 1;
-
-
 }
 
  
